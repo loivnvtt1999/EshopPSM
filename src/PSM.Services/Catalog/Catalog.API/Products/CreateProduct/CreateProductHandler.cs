d64c95aab06c3193e44c1acf7776992f8b75b344
Loi Pham <loi9a2thcsttt@gmail.com>
2025-02-16 23:13:04 +0700

    Add Carter Minimal API
    Add Carter Minimal API

--- src/PSM.Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/PSM.Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,28 @@
-using MediatR;
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProducts;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-: IRequest<CreateProductResult>;
+: ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
 internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        //create Product entity from command obj
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,   
+        };
+        //save to database
+
+        //return CreateProductResult
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
