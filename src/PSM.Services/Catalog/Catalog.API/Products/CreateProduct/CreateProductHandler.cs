c43c74e3a1d84a0d5f9d4a378029ed9811c683c5
Loi Pham <loi9a2thcsttt@gmail.com>
2025-02-16 17:33:26 +0700

    PSM Add BuildingBlock CQRS
    PSM Add BuildingBlock CQRS

---
+++ src/PSM.Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+using MediatR;
+
+namespace Catalog.API.Products.CreateProducts;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+: IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
