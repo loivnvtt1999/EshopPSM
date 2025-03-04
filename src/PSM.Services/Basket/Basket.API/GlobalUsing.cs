229bb2d8a0a67467ada0e77880b3c25d91e56bc4
Loi Pham <loi9a2thcsttt@gmail.com>
2025-03-04 09:40:12 +0700

    PSM Basket API - Add Use Cases
    PSM Basket API - Add Use Cases

---
+++ src/PSM.Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,9 @@
+﻿global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
+global using Carter;
