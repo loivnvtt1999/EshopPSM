d9137735be3af261428feb43e7b59d4f82f5300e
Loi Pham <loi9a2thcsttt@gmail.com>
2025-03-06 07:11:32 +0700

    Initial Discount Database with SQL Lite
    Initial Discount Database with SQL Lite

--- src/PSM.Services/Discount/Discount.gRPC/Program.cs
+++ src/PSM.Services/Discount/Discount.gRPC/Program.cs
@@ -1,9 +1,13 @@
+using Discount.gRPC.Data;
 using Discount.gRPC.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
