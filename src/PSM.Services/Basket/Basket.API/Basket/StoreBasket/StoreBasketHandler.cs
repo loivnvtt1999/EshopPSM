a755dea7091262d16e9fa112379780fa4143a124
Loi Pham <loi9a2thcsttt@gmail.com>
2025-03-09 20:39:34 +0700

    Consuming DiscountgRPC within Basket service
    Consuming DiscountgRPC within Basket service

--- src/PSM.Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/PSM.Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,15 +1,31 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
 
-    public class StoreBasketCommandHandler(IBasketRepository repository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    public class StoreBasketCommandHandler(IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountClient) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
+            //TODO: communicate with Discout.gRPC sevice
+            await DeductDiscount(discountClient, command.Cart, cancellationToken);
+
+            //Store basket in database (use Marten upsert - if exist = update, otherwise = insert)
             await repository.StoreBasket(command.Cart, cancellationToken);
 
             return new StoreBasketResult(command.Cart.UserName);
+        }
+
+        private static async Task DeductDiscount(DiscountProtoService.DiscountProtoServiceClient discountClient, ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            // Communicate with Discount.Grpc and calculate lastest prices of products into sc
+            foreach (var item in cart.Items)
+            {
+                var coupon = await discountClient.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
--- src/PSM.Services/Basket/Basket.API/Program.cs
+++ src/PSM.Services/Basket/Basket.API/Program.cs
@@ -1,53 +1,74 @@
-using Marten;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Discount.Grpc;
 using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+//using BuildingBlocks.Messaging.MassTransit;
+using Marten;
 
-namespace Basket.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//Application Services
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
 {
-    public class Program
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+//Data Services
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "Basket";
+});
+
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
     {
-        public static void Main(string[] args)
-        {
-            var assembly = typeof(Program).Assembly;
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddCarter();
-            builder.Services.AddMediatR(config =>
-            {
-                config.RegisterServicesFromAssembly(assembly);
-                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-            });
-            builder.Services.AddScoped<IBasketRepository, BasketRepository>();
-            builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
-            builder.Services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = builder.Configuration.GetConnectionString("Redis");
-                //options.InstanceName = "Basket";
-            });
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
 
-            //Data Services
-            builder.Services.AddMarten(opts =>
-            {
-                opts.Connection(builder.Configuration.GetConnectionString("Database")!);
-                opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
-            }).UseLightweightSessions();
+    return handler;
+});
 
+//Async Communication Services
+//builder.Services.AddMessageBroker(builder.Configuration);
 
-            builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+//Cross-Cutting Services
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
-            builder.Services.AddHealthChecks()
-                .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
-                .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
-            var app = builder.Build();
-            app.MapCarter();
-            app.UseExceptionHandler(options => { });
-            app.UseHealthChecks("/health",
-            new HealthCheckOptions
-            {
-                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-            });
-            app.Run();
-        }
-    }
-}
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+app.Run();
